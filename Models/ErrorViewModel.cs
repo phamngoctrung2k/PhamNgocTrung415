4c3721b564e1d59ce440e9dccd2c6a84eb28a6ad
phamngoctrung2k <1821050415@student.humg.edu.vn>
2021-12-15 13:10:22 +0700

    first_commit_PNT415

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PhamNgocTrung415.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
